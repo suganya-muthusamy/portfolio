82153c80ed044c80d848a5b7066ab9d579405100
suganya-muthusamy <suganyamuthu9560@gmail.com>
2025-01-07 01:57:56 -0700

    project page Developed

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,32 +1,74 @@
 import React from "react";
+import useProjectData from "../datasets/useProjectData";
+import { p } from "framer-motion/m";
 
 const Projects = () => {
-  const projectList = [
-    {
-      title: "Project One",
-      description: "Description of project one",
-      link: "#",
-    },
-    {
-      title: "Project Two",
-      description: "Description of project two",
-      link: "#",
-    },
-  ];
+  const projectList = useProjectData();
+  console.log(projectList, "project");
 
   return (
-    <div className="projects">
-      <h2>My Projects</h2>
-      <div className="project-cards">
-        {projectList.map((project, index) => (
-          <div key={index} className="project-card">
-            <h3>{project.title}</h3>
-            <p>{project.description}</p>
-            <a href={project.link} target="_blank" rel="noopener noreferrer">
-              View Project
-            </a>
-          </div>
-        ))}
+    <div className="bg-gray-100 py-20">
+      <div className="w-full xl:w-[1140px] px-4 xl:px-0 xl:mx-auto gap-7 h-full">
+        <div className="my-5">
+          <h2 className="text-xl font-bold tracking-wide">My Projects</h2>
+          <p className="text-lg mt-2">
+            Here you will find some of the personal and clients projects that I
+            created with each project containing its own case study
+          </p>
+        </div>
+        <div>
+          {projectList.map((project, i) => {
+            return (
+              <div
+                key={project.id}
+                className="my-20 flex flex-wrap justify-between "
+              >
+                <div className="w-full md:w-6/12 ">
+                  <img
+                    alt={project.img_url}
+                    src={project.img_url}
+                    className="rounded-lg shadow-lg object-cover"
+                  />
+                </div>
+                <div className="w-full md:w-5/12 flex flex-col justify-center ">
+                  <h1 className="text-3xl font-bold">
+                    {project.title}
+                    <span className="text-sm mx-2">{project.type}</span>
+                  </h1>
+
+                  <h2 className="text-gray-600 mt-2">{project.sub_title}</h2>
+                  <i className="text-gray-800 text-lg mt-5">
+                    {project.description.map((desc, i) => {
+                      return <p className="my-2">{desc}</p>;
+                    })}
+                  </i>
+                  <div className="mt-10">
+                    {project.git_url && (
+                      <a
+                        href={project.git_url}
+                        target="_blank"
+                        rel="noreferrer"
+                        className="p-2 bg-night-blue text-white mr-5 rounded-md"
+                      >
+                        Get Code
+                      </a>
+                    )}
+                    {project.live_url && (
+                      <a
+                        href={project.live_url}
+                        target="_blank"
+                        rel="noreferrer"
+                        className="p-2 bg-sand-tan text-white rounded-md"
+                      >
+                        View Live
+                      </a>
+                    )}
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
---
+++ src/datasets/useProjectData.js
@@ -0,0 +1,52 @@
+import React from "react";
+import skipTheKitchen_URL from "./skip-the-kitchen.png";
+import netflixGPT_URL from "./netflix-gpt.png";
+import LMA_URL from "./lakshmi-medical-academy.png";
+
+const useProjectData = () => {
+  const projectList = [
+    {
+      id: 1,
+      type: "Personal",
+      title: "Skip The Kitchen",
+      sub_title:
+        "Clone app inspired from Swiggy, the food delivering web application.",
+      description: [
+        "I re-created some of the frontend features of swiggy's official web app using their live API. Because, I got attracted to their config-driven UI. It was a great experience for me to build this clone website.",
+      ],
+      img_url: skipTheKitchen_URL,
+      git_url: "https://github.com/suganya-muthusamy/Swiggy-Clone",
+      live_url: "https://nomoredish.netlify.app/",
+    },
+    {
+      id: 2,
+      type: "Personal",
+      title: "NetflixGPT",
+      sub_title:
+        "Clone app inspired from Netflix, the movie streaming web application",
+      description: [
+        "I have fetched the real time movie datas from 'The Movie Database(TMDB)",
+        "The search bar is integrated with chatGPT in order to act as a movie recommendation system and implemented multi-lingual searc-hbar.",
+      ],
+      img_url: netflixGPT_URL,
+      git_url: "https://github.com/suganya-muthusamy/netflix-GPT",
+    },
+    {
+      id: 3,
+      type: "Client",
+      title: "Laksmi Medical Academy",
+      sub_title:
+        "An institution giving online courses to medical students based in Tenkasi, India",
+      description: [
+        "In this application I have implemented mutiple choice online examination, where the students can get their result by end of the test and the exam results send to the management.",
+        " It includes a dashboard enabled with user based authentication, and the user able to manage their respective management works.",
+      ],
+
+      img_url: LMA_URL,
+      live_url: "https://lakshmimedicalacademy.com/",
+    },
+  ];
+  return projectList;
+};
+
+export default useProjectData;
